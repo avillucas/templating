d26d4b2a5d46b3e96f10f4d1d52e1b7805609601
Lucas <lucas@dex360.net>
2025-06-03 22:56:12 -0300

    usuarios

--- nodist/migrations/migration-2025-06-03-2130.sql
+++ nodist/migrations/migration-2025-06-03-2130.sql
@@ -1,3 +1,3 @@
-
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `rol`) VALUES
-(1, 'Tester', 'tester@tester.com', '$2b$08$pxPAJEIJg042mM7//GWWCuL06qI5HMbceaql/Etm91NIRwISzsaD2', 'ADMIN');
+(1, 'Tester', 'tester@tester.com', '$2b$10$EkV.fvQHtTGFCcA0yfapmegm3dBqzMzusdtol7Zr6YbpHVfjx/cM.', 'ADMIN'),
+(2, 'Testera', 'testera@tester.com', '$2b$10$i3PW5BjvsrwA3NblolgBBuy8an5i8VueSsPyIMM/IGe1QlTTjbUGy', 'ADMIN');
