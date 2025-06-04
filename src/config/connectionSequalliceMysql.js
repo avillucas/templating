92c4adbe7c42761c91ec40c92e57e49853c6c335
Lucas <lucas@dex360.net>
2025-06-04 20:57:03 -0300

    secualize

---
+++ src/config/connectionSequalliceMysql.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('gdp', 'root', 'password', {
+  host: 'mariadb',
+  dialect:'mariadb'
+});
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
+
+module.exports = {
+  conn: pool.promise()
+}
